79243f362c9eb4de82336b2ff96fa5951c821bb0
armendes <armendes@student.42.fr>
2020-08-10 13:51:47 +0200

    ex05 sauvegarde c07

---
+++ C07/ex05/ft_split.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/10 11:36:13 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/10 13:51:18 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		check_charset(char c, char *charset)
+{
+	int i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	int 	i;
+	int 	j;
+	int		k;
+	char	*result[];
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while (str[i + j])
+	{
+		if (check_charset(str[i + j], charset) == 1)
+		{
+			ft_strncat(result[k], &str[i], j);
+			i++;
+			j = 0;
+			k++;
+		}
+		else
+			j++;
+	}
+	return (result);
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+	char *str = "bonjour";
+	char *charset = "o";
+	char *result[] = ft_split(str, charset);
+
+	int i = 0;
+	while (i < 3)
+	{
+		printf("%s ", result[i]);
+		i++;
+	}
+}
