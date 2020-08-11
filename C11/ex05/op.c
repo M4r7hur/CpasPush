b1e7fbf661b6d3327d62695f663436d3e4f892ba
armendes <armendes@student.42.fr>
2020-08-11 17:50:39 +0200

    C11 ex05 ajout de op.c

---
+++ C11/ex05/op.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   op.c                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/11 17:48:09 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/11 17:50:02 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		plus(int x, int y)
+{
+	return (x + y);
+}
+
+int		minus(int x, int y)
+{
+	return (x - y);
+}
+
+int		divide(int x, int y)
+{
+	return (x / y);
+}
+
+int		times(int x, int y)
+{
+	return (x * y);
+}
+
+int		modulo(int x, int y)
+{
+	return (x % y);
+}
