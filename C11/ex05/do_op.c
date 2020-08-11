aec9a8cb4bfe3549d5555329bd5f6d92830a94a3
armendes <armendes@student.42.fr>
2020-08-11 17:47:23 +0200

    C11 ex05

---
+++ C11/ex05/do_op.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_op.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/11 17:13:06 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/11 17:44:04 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		plus(int x, int y);
+int		minus(int x, int y);
+int		divide(int x, int y);
+int		times(int x, int y);
+int		modulo(int x, int y);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(char *str);
+
+int	ft_atoi(char *str)
+{
+	int i;
+	int sign;
+	int nb;
+
+	i = 0;
+	sign = 1;
+	nb = 0;
+	while (str[i] && (str[i] == 32 || str[i] == '\f' || str[i] == '\n'
+				|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v'))
+		i++;
+	while (str[i] && (str[i] == '-' || str[i] == '+'))
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] && '0' <= str[i] && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nb * sign);
+}
+
+void	do_op(int x1, int x2, char op, int (*f)(int x, int y))
+{
+	if (op == '+')
+		ft_putnbr((*f)[0](x1, x2));
+	if (op == '-')
+		ft_putnbr((*f)[1](x1, x2));
+	if (op == '/')
+		ft_putnbr((*f)[2](x1, x2));
+	if (op == '*')
+		ft_putnbr((*f)[3](x1, x2));
+	if (op == '%')
+		ft_putnbr((*f)[4](x1, x2));
+}
+
+int		main(int argc, char **argv)
+{
+	int	(*operateur[5])(int x, int y);
+
+	operateur[0] = &plus;
+	operateur[1] = &minus;
+	operateur[2] = &divide;
+	operateur[3] = &times;
+	operateur[4] = &modulo;
+	if (argc == 4)
+	{
+		if (argv[2] == '+' || argv[2] == '-' || argv[2] == '/' ||
+				argv[2] == '*' || argv[2] == '%')
+		{
+			if (argv[3] == 0 && argv[2] == '/')
+				ft_putstr("Stop : division by zero");
+			if (argv[3] == 0 && argv[2] == '%')
+				ft_putstr("Stop : modulo by zero");
+			else
+				do_op(ft_atoi(argv[1]), ft_atoi(argv[3]), argv[2], &operateur[]);
+		}
+		ft_putchar('0');
+	}
+	return (0);
+}
---
+++ C11/ex05/put.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   put.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/11 17:44:17 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/11 17:46:32 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + 48);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + 48);
+	}
+}
