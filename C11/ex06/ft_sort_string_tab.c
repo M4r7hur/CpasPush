19e6a786f9d0775bfbbd8e14547c33a1b47b0ebf
armendes <armendes@student.42.fr>
2020-08-13 16:17:38 +0200

    C08 remove et C11 ex06

--- C11/ex06/ft_sort_string_tab.c
+++ C11/ex06/ft_sort_string_tab.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/09 11:46:27 by armendes          #+#    #+#             */
-/*   Updated: 2020/08/09 12:14:22 by armendes         ###   ########.fr       */
+/*   Created: 2020/08/13 14:58:30 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/13 14:59:11 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
 
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
-
-#endif
