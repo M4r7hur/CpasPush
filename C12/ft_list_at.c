aa78f6fedf1f31a27a700ee5894a78c270bef712
armendes <armendes@student.42.fr>
2020-08-14 16:39:00 +0200

    c12

--- C12/ft_list_at.c
+++ C12/ft_list_at.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_list_push_strs.c                                :+:      :+:    :+:   */
+/*   ft_list_at.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/14 11:52:46 by armendes          #+#    #+#             */
-/*   Updated: 2020/08/14 16:38:51 by armendes         ###   ########.fr       */
+/*   Created: 2020/08/14 16:38:16 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/14 16:38:23 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_list.h"
-
-t_list	*ft_list_push_strs(int size, char **strs)
-{
-	int		i;
-	t_list	*elem;
-	t_list	*elem2;
-
-	i = 1;
-	elem = ft_create_elem(strs[0]);
-	while (i < size)
-	{
-		elem2 = ft_create_elem(strs[i]);
-		elem2->next = elem;
-		elem = elem2;
-		i++;
-	}
-	return (elem);
-}
