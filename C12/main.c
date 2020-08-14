beff4954224fdf8064680ca29a28cabde1cbd4f2
armendes <armendes@student.42.fr>
2020-08-14 10:57:38 +0200

    C12

--- C12/main.c
+++ C12/main.c
@@ -6,7 +6,7 @@
 /*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/12 18:02:24 by armendes          #+#    #+#             */
-/*   Updated: 2020/08/14 10:51:16 by armendes         ###   ########.fr       */
+/*   Updated: 2020/08/14 10:57:06 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "ft_list.h"
 
 void	ft_list_push_front(t_list **begin_list, void *data);
+int		ft_list_size(t_list *begin_list);
 
 int main(void)
 {
@@ -23,12 +24,14 @@ int main(void)
 	char *str = "bonjour";
 	element = ft_create_elem(str);
 	printf("%s\n", element->data);
-	
+
 	char *str2 = "bonjour2";
 	ft_list_push_front(&list, str2);
 	printf("%s", list->data);
 
 	char *str3 = "bonjour3";
 	ft_list_push_front(&list, str3);
-	printf("%s", list->data);
+	printf("%s\n", list->data);
+
+	printf("%d", ft_list_size(list));
 }
