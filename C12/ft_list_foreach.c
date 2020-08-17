47c5d5b012854d74476e2936ed289327c63e201d
armendes <armendes@student.42.fr>
2020-08-17 17:49:56 +0200

    retrait de c10 et c11

--- C12/ft_list_foreach.c
+++ C12/ft_list_foreach.c
@@ -6,7 +6,7 @@
 /*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 12:42:41 by armendes          #+#    #+#             */
-/*   Updated: 2020/08/17 14:07:20 by armendes         ###   ########.fr       */
+/*   Updated: 2020/08/17 17:33:17 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
-	t_list	*tmp;
+	t_list	*;
 
 	tmp = begin_list;
 	while (tmp->next)
