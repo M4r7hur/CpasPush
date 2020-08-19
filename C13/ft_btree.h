0219a34d616b5d0dc263d96e07fb6130df00aa0a
armendes <armendes@student.42.fr>
2020-08-19 17:12:01 +0200

    c13

---
+++ C13/ft_btree.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/19 16:59:26 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/19 17:11:49 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BTREE_H
+# define FT_BTREE_H
+
+typedef struct	s_btree
+{
+	struct s_btree	*left;
+	struct s_btree	*right;
+	void			*item;
+}				t_btree;
+
+t_btree *btree_create_node(void *item);
+
+#endif
