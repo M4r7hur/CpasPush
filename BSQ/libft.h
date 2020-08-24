901e38ce7d5ea9f0d508d58fe39471d9af88f22b
armendes <armendes@student.42.fr>
2020-08-24 09:52:59 +0200

    libft.h

---
+++ BSQ/libft.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/24 09:46:07 by armendes          #+#    #+#             */
+/*   Updated: 2020/08/24 09:51:10 by armendes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct	s_list;
+{
+	int				x;
+	int				y;
+	struct s_list	*next;
+	struct s_list	*next_line;
+}				t_list;
+
+
+
+#endif
