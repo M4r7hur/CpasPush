7dbf663b56a8694c183bdf8aaa1560f578d959dd
seciurte <seciurte@student.42.fr>
2020-08-25 21:02:44 +0200

    demain

--- BSQv2/includes/lib.h
+++ BSQv2/includes/lib.h
@@ -1,14 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   lib.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seciurte <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/25 19:18:43 by seciurte          #+#    #+#             */
-/*   Updated: 2020/08/25 19:19:39 by seciurte         ###   ########.fr       */
+/*   Created: 2020/08/25 19:56:46 by seciurte          #+#    #+#             */
+/*   Updated: 2020/08/25 20:21:35 by seciurte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef LIB_H
+# define LIB_H
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -17,3 +20,14 @@
 # include <fcntl.h>
 
 typedef	unsigned long ul;
+typedef struct			s_minfo
+{
+	ul		nbl;
+	ul		nbc;
+	char	**map;
+	char	nobs;
+	char	obs;
+	char	fill;
+}						t_minfo;
+
+#endif
