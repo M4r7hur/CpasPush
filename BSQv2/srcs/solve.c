7a8e227071f96d5f7aeaf71367540ec53702570c
armendes <armendes@student.42.fr>
2020-08-26 19:54:07 +0200

    mise a la norme

--- BSQv2/srcs/solve.c
+++ BSQv2/srcs/solve.c
@@ -6,13 +6,13 @@
 /*   By: armendes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 19:09:36 by armendes          #+#    #+#             */
-/*   Updated: 2020/08/26 18:58:14 by armendes         ###   ########.fr       */
+/*   Updated: 2020/08/26 19:53:42 by armendes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.h"
 
-#include <stdio.h>
+ul		g_maxlen = 0;
 
 ul		ver_x(ul i, ul x, ul len)
 {
@@ -41,7 +41,8 @@ void	disp(ul x, ul y, ul len, t_minfo *info)
 		{
 			if (info->map[i][j] == info->obs)
 				ft_putchar(info->obs);
-			else if (info->map[i][j] == info->nobs && ver_x(i, x, len) && ver_y(j, y, len))
+			else if (info->map[i][j] == info->nobs &&
+					ver_x(i, x, len) && ver_y(j, y, len))
 				ft_putchar(info->fill);
 			else if (info->map[i][j] == info->nobs)
 				ft_putchar(info->nobs);
@@ -52,6 +53,25 @@ void	disp(ul x, ul y, ul len, t_minfo *info)
 	}
 }
 
+void	set_x_y_zero(ul *t1, ul *t2, ul *t3)
+{
+	*t1 = 0;
+	*t2 = 0;
+	*t3 = 0;
+}
+
+void	set_x_zero(ul *t1, ul *t3)
+{
+	*t1 = 0;
+	*t3 = 0;
+}
+
+void	set_y_zero(ul *t1, ul *t2)
+{
+	*t1 = 0;
+	*t2 = 0;
+}
+
 int		check_valid(ul **tab, ul x, ul y, ul len)
 {
 	ul t1;
@@ -60,21 +80,15 @@ int		check_valid(ul **tab, ul x, ul y, ul len)
 	ul t4;
 
 	if (x == 0 && y == 0)
-	{
-		t1 = 0;
-		t2 = 0;
-		t3 = 0;
-	}
+		set_x_y_zero(&t1, &t2, &t3);
 	else if (x == 0 && y != 0)
 	{
-		t1 = 0;
+		set_x_zero(&t1, &t3);
 		t2 = tab[x + len - 1][y - 1];
-		t3 = 0;
 	}
 	else if (x != 0 && y == 0)
 	{
-		t1 = 0;
-		t2 = 0;
+		set_y_zero(&t1, &t2);
 		t3 = tab[x - 1][y + len - 1];
 	}
 	else
@@ -84,50 +98,48 @@ int		check_valid(ul **tab, ul x, ul y, ul len)
 		t3 = tab[x - 1][y + len - 1];
 	}
 	t4 = tab[x + len - 1][y + len - 1];
-	printf("t1= %lu | t2= %lu | t3= %lu | t4= %lu | result= %lu\n", t1, t2, t3, t4, (t4 - t2 - t3 + t1));
 	if ((t4 - t2 - t3 + t1) > 0)
 		return (0);
 	return (1);
 }
 
+void	set_tmp(ul *xtmp, ul x, ul *ytmp, ul y)
+{
+	*xtmp = x;
+	*ytmp = y;
+}
+
+void	set_len(ul *lentmp, ul len)
+{
+	*lentmp = len;
+	g_maxlen = len;
+}
+
 void	solve(ul **tab, t_minfo *info, ul len)
 {
-	ul x;
-	ul y;
-	ul xtmp;
-	ul ytmp;
-	ul lentmp;
-	static ul maxlen;
+	ul			x;
+	ul			y;
+	ul			xtmp;
+	ul			ytmp;
+	ul			lentmp;
 
 	x = 0;
 	y = 0;
-	xtmp = 0;
-	ytmp = 0;
 	lentmp = 0;
 	while ((x + len - 1) < info->nbl && (y + len - 1) < info->nbc)
 	{
-		printf("x= %lu | y= %lu | len= %lu | info->nbl= %lu | info->nbc= %lu | maxlen= %lu | lentmp= %lu\n", x, y, len, info->nbl, info->nbc, maxlen, lentmp);
 		if (check_valid(tab, x, y, len))
 		{
-			//printf("v\n");
-			xtmp = x;
-			ytmp = y;
-			lentmp = len;
-			maxlen = lentmp;
+			set_tmp(&xtmp, x, &ytmp, y);
+			set_len(&lentmp, len);
 			solve(tab, info, len + 1);
 			break ;
 		}
-		if ((y + len - 1) == (info->nbc) - 1)
-		{
+		if ((y + len - 1) == (info->nbc) - 1 && (y = 0) == 0)
 			x++;
-			y = 0;
-		}
 		else
 			y++;
 	}
-	if (lentmp == maxlen)
-	{
-		printf("xtmp= %lu | ytmp= %lu | lentmp= %lu\n", xtmp, ytmp, lentmp);
+	if (lentmp == g_maxlen)
 		disp(xtmp, ytmp, lentmp, info);
-	}
 }
